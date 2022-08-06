18b5497e4d74e4a5ee73c4876a3d7bf97c5d07ab
sadvos <application77863@gmail.com>
2022-08-06 14:25:09 +0100

    feat: ✨ Adds logic to the outgoing checks

--- src/App.vue
+++ src/App.vue
@@ -43,23 +43,47 @@
         socket.send(message);
     }
 
-    export function outgoingChecks(input: socketResponseLayout) {
-    if(input.header === 'CONSOLE-INFORMATION') {
+    export let consoleText: String[] = ['']; // This is the array that holds the console text
+    export let systemTotalMemory: String = '1 TB';
+    export let systemUsedMemory: String = '1 TB';
+    export let systemsRegisteredProgram: String  = 'MongoDB';
+    export let incomingTraffic: Number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // Shows the last 10 didgets of the incoming traffic in people for the graph 
+    export let incomingCPU: Number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    export let incomingMemory: Number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+    export function outgoingChecks(input: socketResponseLayout) {
+    const value: any = input.body; // If statments are just putting the values in the right place
+
+    if(input.header === 'CONSOLE-INFORMATION') {
+        if(consoleText.length >= 25) {
+            consoleText.push(value);
+            consoleText.shift();
+            return;
+        }
+        consoleText.push(value);
     } else if(input.header === 'CONSOLE-ERROR') {
+        if(consoleText.length >= 25) {
+            consoleText.push(value);
+            consoleText.shift();
+            return;
+        }
+        consoleText.push(value);
 
     } else if(input.header === 'SYSTEM-TOTAL-MEMORY-NUMBER') {
-
+        systemTotalMemory = value;
     } else if(input.header === 'SYSTEM-USED-MEMORY-NUMBER') {
-
+        systemUsedMemory = value;
     } else if(input.header === 'SYSTEM-REGISTERED-PROGRAM-INFORMATION') {
-
+        systemsRegisteredProgram = value;
     } else if(input.header === 'CURRENT-INCOMING-TRAFFIC-NUMBER') {
-
+        incomingTraffic.push(value);
+        incomingTraffic.shift();
     } else if(input.header === 'CURRENT-MEMORY-USAGE-NUMBER') {
-
+        incomingCPU.push(value);
+        incomingCPU.shift();
     } else if(input.header === 'CURRENT-CPU-USAGE-NUMBER') {
-
+        incomingMemory.push(value);
+        incomingMemory.shift();
     }
 }
 
